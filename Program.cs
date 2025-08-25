804561aabdd6070da9881202fe2a9753871e5502
abitsiflow <xtiangzmen@gmail.com>
2025-08-25 13:39:42 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿namespace ConsoleApp5
+namespace ConsoleApp5
 {
     internal class Program
     {
@@ -8,10 +8,23 @@
             String fname = "John";
             String lname = "Doe";
 
+            Console.Write("HELLO NEIGHBORHOOD!!!!!!!!!!");
+            Console.Write("");
             Console.WriteLine("Hello, World!");
             Console.WriteLine("I am " + fname + " " + lname);
             Console.WriteLine("I am " + age + " years old!");
             Console.Beep();
+
+            //this is a comment
+            /*
+             * This is a multi line comment
+             * 
+             * 
+             */
+
+            Console.WriteLine("Hello \n \bWorld!!");
+
+            Console.ReadKey();// this method preventing to end the program until the user enter a key to end the program
         }
     }
 }
