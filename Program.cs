c6810ce713247604959b1f912adea5175e576fa9
abitsiflow <xtiangzmen@gmail.com>
2025-08-25 13:25:45 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 10;
+            String fname = "John";
+            String lname = "Doe";
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I am " + fname + " " + lname);
+            Console.WriteLine("I am " + age + " years old!");
+            Console.Beep();
+        }
+    }
+}
