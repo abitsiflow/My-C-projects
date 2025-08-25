ae2bf92679a3436a7f6120ef584ecfbf0eb89000
abitsiflow <xtiangzmen@gmail.com>
2025-08-25 16:17:06 +0800

    Add files via upload

---
+++ Program 1.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           double a = 3.3232;
+            double b = Convert.ToInt32(a);
+            int c = 123;
+            double d = Convert.ToDouble(c) + 0.1;
+
+            Console.WriteLine(b.GetType());
+            Console.WriteLine(d);
+
+            Console.ReadKey();
+        }
+    }
+}
